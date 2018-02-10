bd971798e36a99862511dcf290a31991dbfd6a20
DaniloDaniel <choopper_14_@hotmail.com>
2018-02-10 00:25:41 -0300

    Copia del proyecto.

---
+++ Proyecto_Tesis/Sistema_Reconocimiento_Facial/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Sistema_Reconocimiento_Facial
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbImageRecortada = new System.Windows.Forms.PictureBox();
+            this.pbImagenOriginal = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageRecortada)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenOriginal)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbImageRecortada
+            // 
+            this.pbImageRecortada.Location = new System.Drawing.Point(349, 64);
+            this.pbImageRecortada.Name = "pbImageRecortada";
+            this.pbImageRecortada.Size = new System.Drawing.Size(168, 192);
+            this.pbImageRecortada.TabIndex = 0;
+            this.pbImageRecortada.TabStop = false;
+            // 
+            // pbImagenOriginal
+            // 
+            this.pbImagenOriginal.Location = new System.Drawing.Point(12, 64);
+            this.pbImagenOriginal.Name = "pbImagenOriginal";
+            this.pbImagenOriginal.Size = new System.Drawing.Size(255, 255);
+            this.pbImagenOriginal.TabIndex = 1;
+            this.pbImagenOriginal.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(61, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(137, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "imagen Original 250x250 px";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(362, 270);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(152, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "imagen Recortada 168x192 px";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(635, 391);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbImagenOriginal);
+            this.Controls.Add(this.pbImageRecortada);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pbImageRecortada)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbImagenOriginal)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbImageRecortada;
+        private System.Windows.Forms.PictureBox pbImagenOriginal;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Proyecto_Tesis/Sistema_Reconocimiento_Facial/Form1.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+//Referencias EmguCV
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Emgu.CV.Structure;
+using Emgu.CV.UI;
+using Emgu.CV.Util;
+using Emgu.CV.ML;
+
+
+namespace Sistema_Reconocimiento_Facial
+{
+    public partial class Form1 : Form
+    {
+        //Estas variables serán utilizadas para cargar los datos de entrenamiento y sus respectivas etiquetas (+1, -1)
+        List<Mat> dataTrain;
+        Matrix<float> matrizMat;
+
+        Matrix<int> labelTrain;
+
+        HOGDescriptor desHog;
+        List<List<float>> dataTrainHOG;
+
+        Mat dataTrainMat;
+        int descriptorSize;
+        
+        CascadeClassifier faceDetector;
+        Mat imgMat;
+        SVM model;
+
+        public Form1()
+        {
+            InitializeComponent();
+            loadDatatraining();
+            hog();
+            calculateDescriptorsHOG();
+
+            dataTrainMat = new Mat(new Size((int)dataTrainHOG[0].Count(), dataTrainHOG.Count()), DepthType.Cv32F, 1); //	Mat(Size, DepthType, Int32)
+
+            convertVectorToMatrix();
+            try
+            {
+                //Creación del SVM
+                //Definición de parámetros
+                model = new SVM();
+                model.C = 100;
+                model.Type = SVM.SvmType.CSvc;
+                //model.Coef0 = 0.0; //TODO:Revisar o eliminar
+                //model.Degree = 3; //TODO:Revisar o eliminar
+                //model.Nu = 0.5;//TODO:Revisar o eliminar
+                //model.P = 0.1;//TODO:Revisar o eliminar
+                model.Gamma = 0.005;
+                model.SetKernel(SVM.SvmKernelType.Linear);
+                model.TermCriteria = new MCvTermCriteria(1000, 1e-6);
+                model.Train(dataTrainMat, Emgu.CV.ML.MlEnum.DataLayoutType.RowSample, labelTrain); //<==== BUG posible solucion http://answers.opencv.org/question/93012/svm-training-data-error/
+                model.Save("svm.txt");
+                MessageBox.Show("SVM está entrenada.");
+
+                testIt();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eror: " + ex.Message, "Módulo .:. Form1 ");
+            }
+
+
+            
+
+
+        }
+
+        public static IEnumerable<FileInfo> GetFilesByExtensions(DirectoryInfo dir, params string[] extensions)
+        {
+            if (extensions == null)
+                throw new ArgumentNullException("extensions");
+            IEnumerable<FileInfo> files = dir.EnumerateFiles();
+            return files.Where(f => extensions.Contains(f.Extension));
+        }
+
+        public void loadDatatraining()
+        {
+            faceDetector = new CascadeClassifier("haarcascade_frontalface_default.xml");
+            // Especificar ruta de origen para datos de entrenamiento
+            var path = new DirectoryInfo(Path.Combine(Application.StartupPath, "resources/data-train"));
+            string[] dirsDataTrain = Directory.GetDirectories(@path.ToString());
+            int tamDataTrain = 0;
+            int imgCount = 0;
+            int classID = 0;
+            foreach (var folder in dirsDataTrain)
+            {
+                string[] subDir = Directory.GetFiles(folder.ToString(), "*.jpg");
+                tamDataTrain += (int) subDir.Length;
+            }
+            dataTrain = new List<Mat>();
+            labelTrain = new Matrix<int>(tamDataTrain, 1); //Este valor queda definido según el tamaño de la muestra.
+
+            //Se procede a cargar todas las imágenes de entrenamiento
+            //se utiliza una sentencia de repetición en caso de existir 
+            //una categorización distribuida en carpetas.
+            try
+            {
+
+                foreach (DirectoryInfo classFolder in path.EnumerateDirectories())
+                {
+                    string nameFolder = classFolder.Name;
+                    classID += 1;
+                    // Clasificar la imágenes según su extensión (compresión de imagen)
+                    FileInfo[] files = GetFilesByExtensions(classFolder, ".jpg", ".png").ToArray();
+
+                    // Iterar por carpeta, buscando imágenes
+                    for (int i = 0; i < files.Length; i++)
+                    {
+                        FileInfo file = files[i];  
+                        IImage img = new Mat(file.FullName, ImreadModes.Color);
+                        UMat imgGray = new UMat();
+                        CvInvoke.CvtColor(img, imgGray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                        //Detección de rostros en la una imagen, debería encontrar un sólo rostro por imagen
+                        foreach (Rectangle face in faceDetector.DetectMultiScale(imgGray, 1.1, 10, new Size(20, 20), Size.Empty))
+                        {
+                            CvInvoke.Rectangle(img, face, new MCvScalar(255, 255, 255));
+
+                            //A continuación sigue el proceso de Preprocesado de la imagen
+                            //este consiste en los siguientes pasos: convertir escala de grises, recorte y escalado.
+                            //TODO: ajustar rostro en función del ojo derecho
+
+                            //Conversión de tipo Mat a Image<Brg, Byte>
+                            Image<Bgr, Byte> image = imgGray.ToImage<Bgr, Byte>();
+                            image.ROI = Rectangle.Empty;
+                            //Estableciendo tamaño de región de interés
+                            Rectangle roi = new Rectangle(face.X, face.Y, face.Width, face.Height);
+                            image.ROI = roi;
+                            Image<Bgr, Byte> imgResize = image.Copy(roi);
+                            imgResize = imgResize.Resize(168, 192, Inter.Linear, false);
+                            pbImageRecortada.Image = imgResize.Bitmap;//Mostrando Imagen recortada
+                            pbImagenOriginal.Image = imgGray.Bitmap;//Mostrando Imagen original
+                            imgMat = imgResize.Mat;
+                        }
+                        //Agregando una imagen de tipo Mat a la lista
+                        dataTrain.Add(imgMat);
+                        //Etiquetando datos de entrada
+                        labelTrain[imgCount, 0] = (int)classID;
+                        imgCount++;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eror: "+ex.Message, "Módulo .:. loadDatatraining: ");
+            }
+            Console.WriteLine("Número total de imágenes cargadas: {0}", imgCount);
+
+        }
+
+        public HOGDescriptor hog()
+        {
+            desHog = new HOGDescriptor(new Size(168, 192), new Size(8, 8), new Size(4, 4), new Size(8, 8), 9, 1, -1, 0.2, true);
+            //HOGDescriptor(Size winSize, Size blockSize, Size blockStride, Size cellSize, int nbins = 9, int derivAperture = 1, double winSigma = -1, double L2HysThreshold = 0.2, bool gammaCorrection = true);
+            return desHog;
+        }
+
+        public void calculateDescriptorsHOG()
+        {
+            //Aplicar por imagen de entrenamiento el descriptor HOG
+            try
+            {
+                dataTrainHOG = new List<List<float>>();
+                float[] descriptor;
+
+                for (int y = 0; y < dataTrain.Count; y++)
+                {
+                    List<float> descriptors = new List<float>();
+
+                    Image<Bgr, Byte> imgByte = (dataTrain.ElementAt(y)).ToImage<Bgr, Byte>(); //<---------EXCEPTION
+
+                    descriptor = desHog.Compute(imgByte, new Size(168, 192), new Size(0, 0), null); //Se supone de debería retornar un vector de float vector<float>
+                    descriptors = descriptor.OfType<float>().ToList();//debo convertir float[] to List<float>
+                    dataTrainHOG.Add(descriptors);
+                }
+                //Tamaño de un descriptor
+                descriptorSize = dataTrainHOG[0].Count();                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Eror: " + ex.Message, "Módulo .:. calculateDescriptorsHOG");
+            }
+
+        }
+
+        public void convertVectorToMatrix()
+        {
+
+            try
+            {
+                //Convertir un objeto Mat a Matrix para acceder a sus datos
+                matrizMat= new Matrix<float>(dataTrainMat.Rows, dataTrainMat.Cols, dataTrainMat.NumberOfChannels);               
+                dataTrainMat.CopyTo(matrizMat);
+
+                for (int i = 0; i < dataTrainHOG.Count(); i++)
+                {
+                    for (int j = 0; j < dataTrainHOG[0].Count(); j++)
+                    {
+                        
+                        matrizMat[i, j] = (float) dataTrainHOG[i].ElementAt(j);
+                    }
+                }
+                //Volcar los datos procesados al objeto dataTrainMat
+                //Comprobar que efectivamente se está copiando
+                matrizMat.Mat.CopyTo(dataTrainMat);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show("Error: "+ ex.Message, "Módulo .:. convertVectorToMatrix");
+            }            
+        }
+
+        public void testIt()
+        {
+            try
+            {
+                //TODO: cargar imagen para proceso de testeo
+                List<Mat> dataTest = new List<Mat>();
+
+                IImage img = new Mat(@"C:\Users\DaniloDaniel\Documents\Visual Studio 2015\Projects\Proyecto_Tesis\Sistema_Reconocimiento_Facial\resources\data-test\Aaron_Peirsol_0001.jpg", ImreadModes.Color);
+                UMat imgGray = new UMat();
+                CvInvoke.CvtColor(img, imgGray, Emgu.CV.CvEnum.ColorConversion.Bgr2Gray);
+                Image<Gray, Byte> img2 = imgGray.ToImage<Gray, Byte>();
+                img2 = img2.Resize(168, 192, Inter.Linear);
+                Mat imgMat = new Mat(new Size(168, 192), DepthType.Cv32F, 1);
+                img2.Mat.CopyTo(imgMat);
+                dataTest.Add(imgMat);
+
+                //TODO: Instanciar un objeto de tipo HOG
+                HOGDescriptor testHog = new HOGDescriptor(new Size(168, 192), new Size(8, 8), new Size(4, 4), new Size(8, 8), 9, 1, -1, 0.2, true);
+
+
+                //TODO: Calcular descriptor HOG de la imagen
+                List<List<float>> dataTestHOG = new List<List<float>>();
+                float[] descriptor;
+
+                for (int y = 0; y < dataTest.Count; y++) //Debería tener el valor de 1
+                {
+                    List<float> descriptors = new List<float>();
+
+                    Image<Bgr, Byte> imgByte = (dataTest.ElementAt(y)).ToImage<Bgr, Byte>();
+
+                    descriptor = desHog.Compute(imgByte, new Size(168, 192), new Size(0, 0), null);
+                    descriptors = descriptor.OfType<float>().ToList();
+                    dataTestHOG.Add(descriptors);
+                }
+                int descriptorSizeTest = dataTestHOG[0].Count();
+
+                //TODO: Definir objeto de tipo Mat con formato Cv32F
+                Mat dataTestMat = new Mat(new Size((int)dataTestHOG[0].Count(), dataTestHOG.Count()), DepthType.Cv32F, 1);
+
+
+                //TODO: Convertir Vector a Matrix
+
+                Matrix<float> matrizMat = new Matrix<float>(dataTestMat.Rows, dataTestMat.Cols, dataTestMat.NumberOfChannels);
+                dataTestMat.CopyTo(matrizMat);
+
+                for (int i = 0; i < dataTestHOG.Count(); i++)
+                {
+                    for (int j = 0; j < dataTestHOG[0].Count(); j++)
+                    {
+                        matrizMat[i, j] = (float)dataTestHOG[i].ElementAt(j);
+                    }
+                }
+                matrizMat.Mat.CopyTo(dataTestMat);
+
+                float result = model.Predict(dataTestMat);
+                MessageBox.Show(result.ToString());
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message, "Módulo .:. testIt");
+            }
+        }
+
+    }
+}
